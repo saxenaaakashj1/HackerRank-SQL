936727d32733740cf6dbe6183aac0a74f8d23981
saxenaaakashj1 <saxenaaakashj1@gmail.com>
2024-09-13 23:23:51 +0530

    🚀 first commit 🚀

---
+++ Easy/more_than_75_marks.sql
@@ -0,0 +1,8 @@
+-- Select the 'name' column from the 'students' table
+SELECT name
+FROM students
+-- Filter rows where 'marks' are greater than 75
+WHERE marks > 75
+-- Order the results by the last 3 characters of 'name' in ascending order
+-- and then by 'id' in ascending order
+ORDER BY RIGHT(name, 3) ASC, id ASC;
