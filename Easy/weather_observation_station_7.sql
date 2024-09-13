c492649aac3c7e69504f6b9d1112e9b33e07f7ae
saxenaaakashj1 <saxenaaakashj1@gmail.com>
2024-09-13 22:25:26 +0530

    🚀 first commit 🚀

---
+++ Easy/weather_observation_station_7.sql
@@ -0,0 +1,6 @@
+-- Select distinct city names
+SELECT DISTINCT(city)
+-- From the 'station' table
+FROM station
+-- Filter city names that end with a vowel (a, e, i, o, u) using a regular expression
+WHERE city REGEXP '^.+[aeiou]$';
