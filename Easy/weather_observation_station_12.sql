1510f7b1aedb8bb07a9f97abe66a997d5ea021fa
saxenaaakashj1 <saxenaaakashj1@gmail.com>
2024-09-13 23:12:48 +0530

    🚀 first commit 🚀

---
+++ Easy/weather_observation_station_12.sql
@@ -0,0 +1,6 @@
+-- Select unique city names 
+SELECT DISTINCT(city)
+-- From the station table
+FROM station
+-- Filter cities that start and end with a consonant
+WHERE city REGEXP '^[^aeiou].+[^aeiou]$';
