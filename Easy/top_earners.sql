4a1daea6be85dbb9c2e56ab901ce1fb382fa419a
saxenaaakashj1 <saxenaaakashj1@gmail.com>
2024-09-18 22:31:50 +0530

    🚀 first commit 🚀

---
+++ Easy/top_earners.sql
@@ -0,0 +1,13 @@
+-- Select the maximum total earnings (salary * months) and count the employees with that maximum
+SELECT MAX(salary * months) AS "maximum_total_earnings", 
+    (
+        -- Subquery to count how many employees have the maximum total earnings
+        SELECT COUNT(*) 
+        FROM employee 
+        WHERE (salary * months) = (
+            -- Subquery to find the maximum total earnings
+            SELECT MAX(salary * months) 
+            FROM employee
+        )
+    ) AS "employee_count_with_max_earnings"
+FROM employee;
